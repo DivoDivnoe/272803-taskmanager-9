bc44d2ef7a84bb674e9ddd13f775c509cb5bcf4a
Andrey Ivanov <andrey-tm203@mail.ru>
2019-10-10 20:20:47 +0300

    update filters

--- src/components/filters-template.js
+++ src/components/filters-template.js
@@ -7,6 +7,7 @@ const getFiltersTemplate = (items) => `
       class="filter__input visually-hidden"
       name="filter"
       checked=${!index}
+      ${!filter.count() && `disabled`}
     />
     <label for="filter__${filter.name}" class="filter__label">
       ${filter.name} <span class="filter__${filter.name}-count">${filter.count()}</span></label
